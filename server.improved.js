03634521e91728d31ab9b0ecb7bcf1a017f723d9
Glitch (a3-kristine-guan) <none>
2023-09-19 17:20:12 +0000

    👈💠 Updated with Glitch

---
+++ server.improved.js
@@ -0,0 +1,92 @@
+const http = require( 'http' ),
+      fs   = require( 'fs' ),
+      // IMPORTANT: you must run `npm install` in the directory for this assignment
+      // to install the mime library if you're testing this on your local machine.
+      // However, Glitch will install it automatically by looking in your package.json
+      // file.
+      mime = require( 'mime' ),
+      dir  = 'public/',
+      port = 3000
+
+const appdata = []
+
+
+
+const server = http.createServer( function( request,response ) {
+  if( request.method === 'GET' ) {
+    handleGet( request, response )    
+  }else if( request.method === 'POST' ){
+    handlePost( request, response ) 
+  }
+
+})
+
+const handleGet = function( request, response ) {
+  const filename = dir + request.url.slice( 1 ) 
+
+  if( request.url === '/' ) {
+    sendFile( response, 'public/index.html' )
+  }else{
+    sendFile( response, filename )
+  }
+}
+
+const handlePost = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+
+  request.on( 'end', function() {
+    // console.log("Server")
+    
+    // console.log(appdata)
+    // ... do something with the data here!!!
+    // if /delete, then call delEntry
+    // console.log(request.url)
+    if(request.url === '/submit'){
+      // console.log( JSON.parse(dataString))
+      
+      let newItem = JSON.parse(dataString)
+      if(newItem.toBeDeleted === true){
+        appdata.splice(0,1)
+      }
+      else {
+        appdata.push(newItem)
+      }
+
+    }
+
+    
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.write(JSON.stringify(appdata))
+    response.end()
+  })
+}
+
+
+
+const sendFile = function( response, filename ) {
+   const type = mime.getType( filename ) 
+
+   fs.readFile( filename, function( err, content ) {
+
+     // if the error = null, then we've loaded the file successfully
+     if( err === null ) {
+
+       // status code: https://httpstatuses.com
+       response.writeHeader( 200, { 'Content-Type': type })
+       response.end( content )
+
+     }else{
+
+       // file not found, error code 404
+       response.writeHeader( 404 )
+       response.end( '404 Error: File Not Found' )
+
+     }
+   })
+}
+
+server.listen( process.env.PORT || port )
